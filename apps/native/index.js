43b0f39ee35cdcdfff493b1df2dd66d07566f0fb
fr0staman <mrfrostaman@gmail.com>
2024-03-03 16:05:45 +0200

    fix: ignore "bad setState" logs due to tamagui issue

--- apps/native/index.js
+++ apps/native/index.js
@@ -3,9 +3,12 @@
  */
 import "react-native-gesture-handler";
 
-import { AppRegistry } from "react-native";
+import { AppRegistry, LogBox } from "react-native";
 
 import { name as appName } from "./app.json";
 import App from "./src/App";
+
+// ISSUE: https://github.com/tamagui/tamagui/issues/1828
+LogBox.ignoreLogs([/bad setState/]);
 
 AppRegistry.registerComponent(appName, () => App);
